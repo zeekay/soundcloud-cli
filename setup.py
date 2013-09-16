97e4be26e296b9d943074eee478d9c56d6949838
zeekay <zk@monoid.io>
2013-09-15 19:28:36 -0700

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,21 @@
+import re
+from setuptools import setup, find_packages
+
+file_text = open('sc/__init__.py').read()
+
+def grep(attrname):
+    pattern = r"{0}\W*=\W*'([^']+)'".format(attrname)
+    strval, = re.findall(pattern, file_text)
+    return strval
+
+setup(
+    name='sc',
+    version=grep('__version__'),
+    url='https://github.com/zeekay/sc',
+    description='Soundcloud CLI app',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    scripts=['bin/sc'],
+    install_requires=['soundcloud'],
+)
