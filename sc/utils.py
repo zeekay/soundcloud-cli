b6c42241042c7e0d333638c14de7dd22c9d4bd6e
zeekay <zk@monoid.io>
2013-09-16 04:24:37 -0700

    Copy to clipboard more robustly.

--- sc/utils.py
+++ sc/utils.py
@@ -1,4 +1,13 @@
+import os
+import sys
+
 def copy_to_clipboard(text):
+    # reliable on mac
+    if sys.platform == 'darwin':
+        os.system('echo "{0}" | pbcopy'.format(text))
+        return
+
+    # okay we'll try cross-platform way
     try:
         from Tkinter import Tk
     except ImportError:
@@ -7,7 +16,7 @@ def copy_to_clipboard(text):
     r = Tk()
     r.withdraw()
     r.clipboard_clear()
-    r.clipboard_append(text)
+    r.clipboard_append(text.encode('ascii'))
     r.destroy()
 
 def open_browser(url):
