232740720f8c62b79ffdaea576e2298e263c33f9
Zach Kelling <z@zeekay.io>
2013-09-15 21:00:52 -0700

    Update sc, gitignore.

--- sc/sc.py
+++ sc/sc.py
@@ -1,13 +1,14 @@
 import json
 import os
 import sys
+from datetime import date
 
 import soundcloud
 
 CLIENT_ID     = 'ffc80dc8b5bd435a15f9808724f73c40'
 CLIENT_SECRET = 'b299b6681e00dfd9f5015639c7f5fe29'
 SC_CONF       = os.path.expanduser('~/.sc')
-
+DEFAULT_YEAR  = date.today().year
 
 def get_access_token(username, password):
     client = soundcloud.Client(
@@ -19,68 +20,72 @@ def get_access_token(username, password):
 
     return client.access_token
 
+def get_settings():
+    if get_settings._cached_result:
+        return get_settings._cached_result
 
-def get_client():
     if not os.path.exists(SC_CONF):
         print 'Run auth command to authenticate and save access token.'
         sys.exit(1)
 
     with open(SC_CONF) as f:
-        access_token = json.load(f)['access_token']
+        get_settings._cached_result = settings = json.load(f)
 
-    client = soundcloud.Client(access_token=access_token)
-    return client
+    return settings
 
 
-def compress_track(filename=None, bitrate=320, title='', artist='', album='', year=''):
-    if not filename:
-        raise Exception('Filename required argument')
+def get_client(access_token=None):
+    if not access_token:
+        access_token = get_settings()['access_token']
+
+    return soundcloud.Client(access_token=access_token)
+
+
+def compress_track(filename, artist=None, title='', album='', year=DEFAULT_YEAR, bitrate=320):
+    if not artist:
+        artist = get_settings()['username']
 
     os.system('lame -b %d --tt "%s" --ta "%s" --tl "%s" --ty %s %s' % (bitrate, title, artist, album, year, filename))
 
 
-def upload_track(filename=None, title=None, private=True):
+def upload_track(filename, title=None, sharing='private'):
     filename = os.path.expanduser(filename)
-
-    if not filename:
-        raise Exception('Filename required argument')
-
     client = get_client()
 
     if not title:
         title = os.path.splitext(os.path.basename(filename))[0]
 
-    track = {
-        'title': title,
-        'asset_data': open(filename, 'rb')
-    }
+    with open(filename, 'rb') as f:
+        track = client.post('/tracks', track={
+            'title': title,
+            'asset_data': f,
+            'sharing': sharing,
+        })
 
-    if private:
-        track['sharing'] = 'private'
-
-    print track
-    track = client.post('/tracks', track=track)
+    if sharing == 'private':
+        secret_token = track.secret_uri.split('secret_token=')[0]
+        track.permalink_url = track.permalink_url + '/' + secret_token
 
     return track
 
 
 def command_upload(args):
-    if args.compress:
-        compress_track(
-            filename=args.filename,
-            bitrate=args.bitrate,
-            title=args.title,
-            artist=args.artist,
-            album=args.album,
-            year=args.year
-        )
+    if args.filename.endswith('.wav'):
+        compress_track(args.filename, bitrate=args.bitrate,
+                                      title=args.title,
+                                      artist=args.artist,
+                                      album=args.album,
+                                      year=args.year)
+
         args.filename.replace('.wav', '.mp3')
 
-    upload_track(
-        filename=args.filename,
-        title=args.title,
-        private=args.private
-    )
+    if args.public:
+        sharing = 'public'
+    else:
+        sharing = 'private'
+
+    track = upload_track(args.filename, title=args.title, sharing=sharing)
+    print track.permalink_url
 
 
 def command_auth(args):
@@ -94,32 +99,35 @@ def command_auth(args):
     password = getpass.getpass('Enter password: ')
 
     with open(SC_CONF, 'w') as f:
-        json.dump({'access_token': get_access_token(username, password)}, f)
+        json.dump({'username': username, 'access_token': get_access_token(username, password)}, f)
 
     print 'Saved access_token.'
 
+
 def main():
     import argparse
-    from datetime import date
-
-    args = sys.argv
 
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers()
 
     upload_parser = subparsers.add_parser('upload', help='Upload track to soundcloud')
     upload_parser.add_argument('filename', action='store', help='File to upload')
-    upload_parser.add_argument('--private', action='store_true', help='Make track private')
-    upload_parser.add_argument('--compress', action='store_true', help='Compress file')
+    upload_parser.add_argument('--public', action='store_true', help='Make track public')
+    upload_parser.add_argument('--no-compress', action='store_false', help='Compress file')
     upload_parser.add_argument('--bitrate', default=320, help='Compress file')
     upload_parser.add_argument('--tags', help='Tags for track')
+    upload_parser.add_argument('--artist', help='id3 title')
     upload_parser.add_argument('--title', help='id3 title')
     upload_parser.add_argument('--album', help='id3 album')
-    upload_parser.add_argument('--year', default=date.today().year, help='id3 year')
+    upload_parser.add_argument('--year', default=DEFAULT_YEAR, help='id3 year')
     upload_parser.set_defaults(command=command_upload)
 
     auth_parser = subparsers.add_parser('auth', help='Authenticate and save access token')
     auth_parser.set_defaults(command=command_auth)
+
+    # default to upload command
+    if sys.argv[1:] and sys.argv[1] not in subparsers.choices:
+        sys.argv = [sys.argv[0], 'upload'] + sys.argv[1:]
 
     args = parser.parse_args()
     args.command(args)
