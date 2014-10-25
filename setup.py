9830e7ad54f2257a81baf90ee99cb77ac5629d88
zeekay <zk@monoid.io>
2014-10-25 14:28:04 -0500

    Update setup.py.

--- setup.py
+++ setup.py
@@ -19,12 +19,17 @@ setup(
     zip_safe=False,
     scripts=['bin/sc', 'bin/soundcloud-cli'],
     install_requires=['soundcloud'],
-    license='LICENSE',
+    license='MIT',
     description='Soundcloud command-line utility',
     long_description=open('README.rst').read(),
     classifiers=[
+        'Development Status :: 5 - Production/Stable',
+
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+
+        'License :: OSI Approved :: MIT License',
     ],
+    keywords='commandline cli soundcloud music upload utility',
 )
