308ba145e8c6fb9be3aed5acdd577d0c26735d02
zeekay <zk@monoid.io>
2014-10-25 14:16:30 -0500

    Update setup.py.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import re
 from setuptools import setup, find_packages
 
-file_text = open('sc/__init__.py').read()
+file_text = open('soundcloud_cli/__init__.py').read()
 
 def grep(attrname):
     pattern = r"{0}\W*=\W*'([^']+)'".format(attrname)
@@ -9,15 +9,15 @@ def grep(attrname):
     return strval
 
 setup(
-    name='sc-cli',
+    name='soundcloud-cli',
     version=grep('__version__'),
     author='Zach Kelling',
     author_email='zk@monoid.io',
-    url='https://github.com/zeekay/sc',
+    url='https://github.com/zeekay/soundcloud-cli',
     description='Soundcloud CLI app',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    scripts=['bin/sc'],
+    scripts=['bin/sc', 'bin/soundcloud-cli'],
     install_requires=['soundcloud'],
 )
